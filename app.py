b5aedf03d2937f7d2f18d94d5f46e1b94baf6245
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2016-01-09 01:03:48 +0500

    Everything working. I also downloaded some files.

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+"""
+    Requirements:
+    - requests (installation: pip install requests)
+    - lxml (installation: pip install lxml)
+"""
+
 import requests
 import lxml.html
 import os
@@ -12,11 +19,14 @@ def download_file(file_name, url):
                 f.write(chunk)
     return file_name
 
-for i in range(1,1381):
-	url = "http://www.europarl.europa.eu/RegistreWeb/search/typedoc.htm?codeTypeDocu=QECR&year=2015&currentPage={0}".format(i)
-	html = lxml.html.parse(url)
-	title = [i.strip() for i in html.xpath("//div[contains(@class, 'notice')]/p[@class='title']/a/text()")]
-	doc = [i.strip() for i in html.xpath("//div[contains(@class, 'notice')]/ul/li/a/@href")]
-	q_ref = [i.strip() for i in html.xpath("//div[contains(@class, 'notice')]/div[@class='date_reference']/span[2]/text()")]
-	
 
+for i in range(1, 1381):
+    url = "http://www.europarl.europa.eu/RegistreWeb/search/typedoc.htm?codeTypeDocu=QECR&year=2015&currentPage={0}".format(i)
+    html = lxml.html.parse(url)
+    titles = [i.strip() for i in html.xpath("//div[contains(@class, 'notice')]/p[@class='title']/a/text()")]
+    docs = [i.strip() for i in html.xpath("//div[contains(@class, 'notice')]/ul/li/a/@href")]
+    q_refs = [i.strip() for i in html.xpath("//div[contains(@class, 'notice')]/div[@class='date_reference']/span[2]/text()")]
+    for title, doc, q_ref in zip(titles, docs, q_refs):
+        file_name = os.path.join(os.getcwd(),'data','-'.join(title.split('/'))+' '+q_ref+'.'+doc.split('.')[-1])
+        downloaded_file = download_file(file_name, doc)
+        print downloaded_file
